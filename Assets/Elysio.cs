1318b9547b208a4c3f7ec7fb190b7cad08b1b606
A11v1r15 <a11v1r15@Rocketmail.com>
2016-10-07 16:00:07 -0300

    Rota de colisão

---
+++ Assets/Elysio.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Elysio : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		this.transform.Translate (Vector3.forward/100);
+	}
+}
