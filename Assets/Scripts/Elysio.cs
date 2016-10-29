883060e64559dea36bb122308eff7d793e1fe0a7
A11v1r15 <a11v1r15@Rocketmail.com>
2016-10-28 23:36:08 -0300

    1, 2, 3, um passito pra frente Maria, 1, 2, 3, um passito pra trás

--- Assets/Scripts/Elysio.cs
+++ Assets/Scripts/Elysio.cs
@@ -3,13 +3,23 @@ using System.Collections;
 
 public class Elysio : MonoBehaviour {
 
+
+	/** Para acesso mais eficiente ao componente Animator
+	 */
+	private Animator animator;
+	private Rigidbody rigidBody;
+
 	// Use this for initialization
 	void Start () {
-	
+		animator = GetComponent<Animator> ();
+		rigidBody = GetComponent<Rigidbody> ();
 	}
 	
 	// Update is called once per frame
 	void FixedUpdate () {
-		this.transform.Translate (Vector3.forward/100);
+		transform.localPosition += Vector3.left * Input.GetAxis ("Horizontal") * Time.deltaTime;
+		if (Input.GetAxis ("Vertical") > 0) {
+			rigidBody.AddForce (Vector3.up * 20);
+		}
 	}
 }
