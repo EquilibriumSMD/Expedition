192e565211644f058c9e7461eea4f8d71b4e9538
A11v1r15 <a11v1r15@Rocketmail.com>
2016-10-30 17:16:25 -0300

    Vuforia

--- Assets/Scripts/Elysio.cs
+++ Assets/Scripts/Elysio.cs
@@ -8,18 +8,22 @@ public class Elysio : MonoBehaviour {
 	 */
 	private Animator animator;
 	private Rigidbody rigidBody;
+	private Collider collider;
 
 	// Use this for initialization
 	void Start () {
 		animator = GetComponent<Animator> ();
+		collider = GetComponent<Collider> ();
 		rigidBody = GetComponent<Rigidbody> ();
 	}
 	
 	// Update is called once per frame
 	void FixedUpdate () {
 		transform.localPosition += Vector3.left * Input.GetAxis ("Horizontal") * Time.deltaTime;
-		if (Input.GetAxis ("Vertical") > 0) {
-			rigidBody.AddForce (Vector3.up * 20);
+		if (Input.GetAxis ("Horizontal") != 0) {
+			animator.SetBool("Walking",true);
+		} else {
+			animator.SetBool("Walking",false);
 		}
 	}
 }
