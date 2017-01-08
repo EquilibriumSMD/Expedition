b62f692a771bb83fd0826b0aa397fe8d029450ed
isabelasilveira <isabela.wdev@gmail.com>
2017-01-08 17:21:43 -0200

    Botões criados

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -1,4 +1,4 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
@@ -10,8 +10,6 @@ public class Menu : MonoBehaviour {
  public Button BotaoJogar,BotaoBiblioteca,BotaoConfigurar,BotaoSair;
  [Space(20)]
  public Slider BarraVolume;
- public Toggle CaixaModoJanela;
- public Dropdown Resolucoes, Qualidades;
  public Button BotaoVoltar, BotaoSalvarPref;
  [Space(20)]
  public Text textoVol;
@@ -19,165 +17,77 @@ public class Menu : MonoBehaviour {
  public string livros = "SceneBooks";
  private string nomeDaCena;
  private float VOLUME;
- private int qualidadeGrafica, modoJanelaAtivo, resolucaoSalveIndex;
- private bool telaCheiaAtivada;
- private Resolution[] resolucoesSuportadas;
 
  void Awake(){
- DontDestroyOnLoad (transform.gameObject);
- resolucoesSuportadas = Screen.resolutions;
+    DontDestroyOnLoad (transform.gameObject);
  }
 
  void Start () {
- Opcoes (false);
- ChecarResolucoes ();
- AjustarQualidades ();
- //
- if (PlayerPrefs.HasKey ("RESOLUCAO")) {
- int numResoluc = PlayerPrefs.GetInt ("RESOLUCAO");
- if (resolucoesSuportadas.Length <= numResoluc) {
- PlayerPrefs.DeleteKey ("RESOLUCAO");
- }
- }
- //
- nomeDaCena = SceneManager.GetActiveScene ().name;
- Cursor.visible = true;
- Time.timeScale = 1;
- //
- BarraVolume.minValue = 0;
- BarraVolume.maxValue = 1;
+     Opcoes (false);
+     nomeDaCena = SceneManager.GetActiveScene ().name;
+     Cursor.visible = true;
+     Time.timeScale = 1;
+     BarraVolume.minValue = 0;
+     BarraVolume.maxValue = 1;
 
- //=============== SAVES===========//
- if (PlayerPrefs.HasKey ("VOLUME")) {
- VOLUME = PlayerPrefs.GetFloat ("VOLUME");
- BarraVolume.value = VOLUME;
- } else {
- PlayerPrefs.SetFloat ("VOLUME", 1);
- BarraVolume.value = 1;
- }
- //=============MODO JANELA===========//
- if (PlayerPrefs.HasKey ("modoJanela")) {
- modoJanelaAtivo = PlayerPrefs.GetInt ("modoJanela");
- if (modoJanelaAtivo == 1) {
- Screen.fullScreen = false;
- CaixaModoJanela.isOn = true;
- } else {
- Screen.fullScreen = true;
- CaixaModoJanela.isOn = false;
- }
- } else {
- modoJanelaAtivo = 0;
- PlayerPrefs.SetInt ("modoJanela", modoJanelaAtivo);
- CaixaModoJanela.isOn = false;
- Screen.fullScreen = true;
- }
- //========RESOLUCOES========//
- if (modoJanelaAtivo == 1) {
- telaCheiaAtivada = false;
- } else {
- telaCheiaAtivada = true;
- }
- if (PlayerPrefs.HasKey ("RESOLUCAO")) {
- resolucaoSalveIndex = PlayerPrefs.GetInt ("RESOLUCAO");
- Screen.SetResolution(resolucoesSuportadas[resolucaoSalveIndex].width,resolucoesSuportadas[resolucaoSalveIndex].height,telaCheiaAtivada);
- Resolucoes.value = resolucaoSalveIndex;
- } else {
- resolucaoSalveIndex = (resolucoesSuportadas.Length -1);
- Screen.SetResolution(resolucoesSuportadas[resolucaoSalveIndex].width,resolucoesSuportadas[resolucaoSalveIndex].height,telaCheiaAtivada);
- PlayerPrefs.SetInt ("RESOLUCAO", resolucaoSalveIndex);
- Resolucoes.value = resolucaoSalveIndex;
- }
- //=========QUALIDADES=========//
- if (PlayerPrefs.HasKey ("qualidadeGrafica")) {
- qualidadeGrafica = PlayerPrefs.GetInt ("qualidadeGrafica");
- QualitySettings.SetQualityLevel(qualidadeGrafica);
- Qualidades.value = qualidadeGrafica;
- } else {
- QualitySettings.SetQualityLevel((QualitySettings.names.Length-1));
- qualidadeGrafica = (QualitySettings.names.Length-1);
- PlayerPrefs.SetInt ("qualidadeGrafica", qualidadeGrafica);
- Qualidades.value = qualidadeGrafica;
- }
+     //=============== SAVES===========//
+     if (PlayerPrefs.HasKey ("VOLUME")) {
+         VOLUME = PlayerPrefs.GetFloat ("VOLUME");
+         BarraVolume.value = VOLUME;
+     } else {
+         PlayerPrefs.SetFloat ("VOLUME", 1);
+         BarraVolume.value = 1;
+     }
 
- // =========SETAR BOTOES==========//
- BotaoJogar.onClick = new Button.ButtonClickedEvent();
- BotaoBiblioteca.onClick = new Button.ButtonClickedEvent();
- BotaoConfigurar.onClick = new Button.ButtonClickedEvent();
- BotaoSair.onClick = new Button.ButtonClickedEvent();
- BotaoVoltar.onClick = new Button.ButtonClickedEvent();
- BotaoSalvarPref.onClick = new Button.ButtonClickedEvent();
- BotaoJogar.onClick.AddListener(() => Jogar());
- BotaoConfigurar.onClick.AddListener(() => Opcoes(true));
- BotaoSair.onClick.AddListener(() => Sair());
- BotaoVoltar.onClick.AddListener(() => Opcoes(false));
- BotaoSalvarPref.onClick.AddListener(() => SalvarPreferencias());
+     // =========SETAR BOTOES==========//
+     BotaoJogar.onClick = new Button.ButtonClickedEvent();
+     BotaoBiblioteca.onClick = new Button.ButtonClickedEvent();
+     BotaoConfigurar.onClick = new Button.ButtonClickedEvent();
+     BotaoSair.onClick = new Button.ButtonClickedEvent();
+     BotaoVoltar.onClick = new Button.ButtonClickedEvent();
+     BotaoSalvarPref.onClick = new Button.ButtonClickedEvent();
+     BotaoJogar.onClick.AddListener(() => Jogar());
+     BotaoConfigurar.onClick.AddListener(() => Opcoes(true));
+     BotaoSair.onClick.AddListener(() => Sair());
+     BotaoVoltar.onClick.AddListener(() => Opcoes(false));
+     BotaoSalvarPref.onClick.AddListener(() => SalvarPreferencias());
  }
- //=========VOIDS DE CHECAGEM==========//
- private void ChecarResolucoes(){
- Resolution[] resolucoesSuportadas = Screen.resolutions;
- Resolucoes.options.Clear ();
- for(int y = 0; y < resolucoesSuportadas.Length; y++){
- Resolucoes.options.Add(new Dropdown.OptionData() { text = resolucoesSuportadas[y].width + "x" + resolucoesSuportadas[y].height });
- }
- Resolucoes.captionText.text = "Resolucao";
- }
- private void AjustarQualidades(){
- string[] nomes = QualitySettings.names;
- Qualidades.options.Clear ();
- for(int y = 0; y < nomes.Length; y++){
- Qualidades.options.Add(new Dropdown.OptionData() { text = nomes[y] });
- }
- Qualidades.captionText.text = "Qualidade";
- }
+    
+//=========VOIDS DE CHECAGEM==========//
  private void Opcoes(bool ativarOP){
- BotaoJogar.gameObject.SetActive (!ativarOP);
- BotaoBiblioteca.gameObject.SetActive (!ativarOP);
- BotaoConfigurar.gameObject.SetActive (!ativarOP);
- BotaoSair.gameObject.SetActive (!ativarOP);
- //
- textoVol.gameObject.SetActive (ativarOP);
- BarraVolume.gameObject.SetActive (ativarOP);
- CaixaModoJanela.gameObject.SetActive (ativarOP);
- Resolucoes.gameObject.SetActive (ativarOP);
- Qualidades.gameObject.SetActive (ativarOP);
- BotaoVoltar.gameObject.SetActive (ativarOP);
- BotaoSalvarPref.gameObject.SetActive (ativarOP);
+     BotaoJogar.gameObject.SetActive (!ativarOP);
+     BotaoBiblioteca.gameObject.SetActive (!ativarOP);
+     BotaoConfigurar.gameObject.SetActive (!ativarOP);
+     BotaoSair.gameObject.SetActive (!ativarOP);
+     textoVol.gameObject.SetActive (ativarOP);
+     BarraVolume.gameObject.SetActive (ativarOP);
+     BotaoVoltar.gameObject.SetActive (ativarOP);
+     BotaoSalvarPref.gameObject.SetActive (ativarOP);
  }
- //=========VOIDS DE SALVAMENTO==========//
+    
+//=========VOIDS DE SALVAMENTO==========//
  private void SalvarPreferencias(){
- if (CaixaModoJanela.isOn == true) {
- modoJanelaAtivo = 1;
- telaCheiaAtivada = false;
- } else {
- modoJanelaAtivo = 0;
- telaCheiaAtivada = true;
- }
- PlayerPrefs.SetFloat ("VOLUME", BarraVolume.value);
- PlayerPrefs.SetInt ("qualidadeGrafica", Qualidades.value);
- PlayerPrefs.SetInt ("modoJanela", modoJanelaAtivo);
- PlayerPrefs.SetInt ("RESOLUCAO", Resolucoes.value);
- resolucaoSalveIndex = Resolucoes.value;
- AplicarPreferencias ();
+     PlayerPrefs.SetFloat ("VOLUME", BarraVolume.value);
+     AplicarPreferencias ();
  }
  private void AplicarPreferencias(){
- VOLUME = PlayerPrefs.GetFloat ("VOLUME");
- QualitySettings.SetQualityLevel(PlayerPrefs.GetInt ("qualidadeGrafica"));
- Screen.SetResolution(resolucoesSuportadas[resolucaoSalveIndex].width,resolucoesSuportadas[resolucaoSalveIndex].height,telaCheiaAtivada);
+     VOLUME = PlayerPrefs.GetFloat ("VOLUME");
  }
- //===========VOIDS NORMAIS=========//
+    
+//===========VOIDS NORMAIS=========//
  void Update(){
- if (SceneManager.GetActiveScene ().name != nomeDaCena) {
- AudioListener.volume = VOLUME;
- Destroy (gameObject);
- }
+     if (SceneManager.GetActiveScene ().name != nomeDaCena) {
+         AudioListener.volume = VOLUME;
+         Destroy (gameObject);
+     }
  }
  private void Jogar(){
- SceneManager.LoadScene (nomeCenaJogo);
+    SceneManager.LoadScene (nomeCenaJogo);
  }
- //private void Biblioteca(){
- //SceneManager.LoadScene (SceneBooks);
- //}
+ private void Biblioteca(){
+    SceneManager.LoadScene (SceneBooks);
+ }
  private void Sair(){
- Application.Quit ();
+    Application.Quit ();
  }
 }
