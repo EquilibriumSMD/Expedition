6c5f06b0a0b50fc333b74fef53bdf855f6769890
jposawa <jp.osawa@hotmail.com>
2017-01-09 15:52:23 -0200

    Alterados Parâmetros do Pulo
    Alterado o JumpSpeed e Delay. Em testes aqui os novos valores padrão tornaram o pulo melhor e evitou o salto duplo que estava estranho

--- Assets/Scripts/Move.cs
+++ Assets/Scripts/Move.cs
@@ -6,11 +6,11 @@ using System.Timers;
 public class Move : MonoBehaviour {
 	public bool isGround;
 	public bool goingUp; // true going up, false going down
-	public float jumpDelay = 0.3f; // stop fast double jumps
+	public float jumpDelay = 1.5f; // stop fast double jumps [Osawa]: Com 1.5f ficou melhor pois com o valor anterior de 0.3 tava muito rápido e causava salto duplo
 	public float inactiveTime; // if action time is less than this time, can't do action
 	public float actionTime;
 	public float rayDistance = 0.1f; // distance center to ground
-	public float jumpSpeed = 7f;
+	public float jumpSpeed = 5f; // [Osawa]: Torna o salto menos "rápido", ficou mais orgânico aqui
 
 	public Vector3 newCenterRun;
 
@@ -53,13 +53,21 @@ public class Move : MonoBehaviour {
 			if (Input.GetAxis ("Vertical") < 0) {
 				animator.SetInteger ("Crouched", 1);
 			}
+            else
+            {
+                animator.SetInteger("Crouched", 0); //Retornar a andar ereto mesmo em movimento
+            }
 			animator.SetBool("Walking",true);
 			transform.rotation = new Quaternion (0, -90, 0, 90);
 		} else if (Input.GetAxis ("Horizontal") < 0) {
 			if (Input.GetAxis ("Vertical") < 0) {
 				animator.SetInteger ("Crouched", -1);
 			}
-			animator.SetBool("Walking",true);
+            else
+            {
+                animator.SetInteger("Crouched", 0);
+            }
+            animator.SetBool("Walking",true);
 			transform.rotation = new Quaternion (0, 90, 0, 90);
 		} else{
 			if (Input.GetAxisRaw ("Vertical") < 0) {
