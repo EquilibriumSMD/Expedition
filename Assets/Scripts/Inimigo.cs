b1256db50e63863d59045b0f8596ea16835917b6
A11v1r15 <a11v1r15@Rocketmail.com>
2017-01-11 16:43:35 -0300

    Inimigo + Android

---
+++ Assets/Scripts/Inimigo.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Inimigo : MonoBehaviour {
+
+
+	/** Para acesso mais eficiente ao componente Animator
+	 */
+	private Animator animator;
+	private Rigidbody rigidBody;
+	private Collider collider;
+	public Vector3 startPosition;
+	public float radius;
+
+	// Use this for initialization
+	void Start () {
+		animator = GetComponent<Animator> ();
+		collider = GetComponent<Collider> ();
+		rigidBody = GetComponent<Rigidbody> ();
+		startPosition = transform.localPosition;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.localPosition = startPosition + Vector3.left *  Mathf.Sin(Time.time) * radius * Time.deltaTime;
+	}
+}
