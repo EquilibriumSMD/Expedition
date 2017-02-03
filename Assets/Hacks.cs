1ffbe3f10eb27f10716db7b5789e4fd1e6f7b6a8
A11v1r15 <a11v1r15@Rocketmail.com>
2017-02-03 01:50:49 -0300

    É Tetra‼

---
+++ Assets/Hacks.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Hacks : MonoBehaviour {
+
+	public Component[] children;
+	public Component[] hottdren;
+
+	// Use this for initialization
+	void Start () {
+		children = GetComponentsInChildren<Transform>( );
+		hottdren = GameObject.Find ("scene1_modificada").GetComponentsInChildren<Transform> ();
+
+		foreach (Transform child in children) {
+			foreach (Transform hottd in hottdren) {
+				if (child.name == hottd.name) {
+					child.transform.position = hottd.transform.position;
+				}
+			}			
+		}
+	}
+}
